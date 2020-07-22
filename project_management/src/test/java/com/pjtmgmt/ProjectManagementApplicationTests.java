a7d34ca5e18245a4c4a5c9a108b39c23e88109ca
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-22 17:09:43 +0530

    initial commit

---
+++ project_management/src/test/java/com/pjtmgmt/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjtmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
