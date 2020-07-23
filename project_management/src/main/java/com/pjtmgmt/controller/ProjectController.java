8fbfb78574ea10d8fabf2420b3cca790db80a9aa
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-23 11:15:11 +0530

    Project object validation

--- project_management/src/main/java/com/pjtmgmt/controller/ProjectController.java
+++ project_management/src/main/java/com/pjtmgmt/controller/ProjectController.java
@@ -1,14 +1,18 @@
 package com.pjtmgmt.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pjtmgmt.model.Project;
+import com.pjtmgmt.service.MapValidationErrorService;
 import com.pjtmgmt.service.ProjectService;
 
 @RestController
@@ -18,8 +22,15 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+	
 	@PostMapping("/")
-	public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+		
+		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+		if(errorMap != null) return errorMap;
+		
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 	}
