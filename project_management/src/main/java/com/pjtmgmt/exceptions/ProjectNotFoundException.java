ac7ba7438735ab40217ea145a405938d1ce08aff
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-24 17:43:57 +0530

    handling project not found exception

---
+++ project_management/src/main/java/com/pjtmgmt/exceptions/ProjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.pjtmgmt.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class ProjectNotFoundException extends RuntimeException{
+
+	public ProjectNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ project_management/src/main/java/com/pjtmgmt/exceptions/ProjectNotFoundExceptionResponse.java
@@ -0,0 +1,22 @@
+package com.pjtmgmt.exceptions;
+
+public class ProjectNotFoundExceptionResponse {
+
+	private String projectNotFound;
+
+	public ProjectNotFoundExceptionResponse(String projectNotFound) {
+		super();
+		this.projectNotFound = projectNotFound;
+	}
+
+	public String getProjectNotFound() {
+		return projectNotFound;
+	}
+
+	public void setProjectNotFound(String projectNotFound) {
+		this.projectNotFound = projectNotFound;
+	}
+	
+	
+	
+}
